18410cb5a4459dd5d56d4096fab2d81ab9150c13
Prakhar Gupta <mywolfdale@gmail.com>
2016-06-12 10:25:55 +0530

    Update main_gui.py

--- main_gui.py
+++ main_gui.py
@@ -26,3 +26,32 @@ class App(object):
         self.label.pack()
 
         self.root.mainloop()
+    def clicked1(self):
+        hex_code = self.entrytext.get()
+	hex_code = '#' + str(hex_code)
+        self.hextocolor(str(hex_code))
+        #self.label.configure(text=result)
+	self.image_area = Canvas(self.root, width = 100, height = 100)
+	self.image_area.pack()
+	PILFile = Image.open('image.jpg')
+	self.image_area.photo = ImageTk.PhotoImage(PILFile)
+	self.image_area.delete('all')
+	self.image_area.create_image(80,80,image=self.image_area.photo,anchor=NW)	
+
+    def hextocolor(self, hex_code):
+        rgb_tuple = ImageColor.getrgb(hex_code)
+        image = Image.new('RGB',(120,120))
+        color_list = []
+        color_list.append(rgb_tuple)
+        color_list = color_list * 14400
+        image.putdata(color_list)
+        image.save("image.jpg")
+
+
+    def button_click(self, e):
+        pass
+
+
+
+if __name__=='__main__':
+	App()
